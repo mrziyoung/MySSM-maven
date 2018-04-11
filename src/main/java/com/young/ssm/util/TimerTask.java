bc0b181205a626054ccae5657a7c9b467690128f
mrziyoung <mr_ziyoung@sina.cn>
2018-04-11 10:04:23 +0800

    modify some unreasonable codes.

--- src/main/java/com/young/ssm/util/TimerTask.java
+++ src/main/java/com/young/ssm/util/TimerTask.java
@@ -12,7 +12,9 @@ public class TimerTask {
     public  void execute() {
         long ms = System.currentTimeMillis();
         System.out.print("定时任务开始执行：");
+        counter++;
         System.out.println("\t\t" + new Date(ms));
         System.out.println("定时任务已执行"+counter+"次");
+
     }
 }

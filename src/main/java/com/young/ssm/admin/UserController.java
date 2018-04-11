9fca6824b2ab417342f76697d60f71a1fbe8d9a0
mrziyoung <mr_ziyoung@sina.cn>
2018-04-11 10:15:29 +0800

    change to RESTFul,add some annotations and the type of response.

--- src/main/java/com/young/ssm/admin/UserController.java
+++ src/main/java/com/young/ssm/admin/UserController.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.young.ssm.common.Result;
+import com.young.ssm.common.ResultGenerator;
 import com.young.ssm.entity.User;
 import com.young.ssm.service.UserService;
 import com.young.ssm.util.MD5Util;
@@ -20,7 +22,9 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.MDC;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 /**
@@ -42,9 +46,11 @@ public class UserController {
      * @param user
      * @param request
      * @return
+     * @warning  灰色代码是之前使用传统请求.do时候的代码
      */
-    @RequestMapping("/login")
-    public String login(User user, HttpServletRequest request) {
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    @ResponseBody
+    public Result login(User user, HttpServletRequest request) {
         try {
             String MD5pwd = MD5Util.MD5Encode(user.getPassword(), "UTF-8");
             user.setPassword(MD5pwd);
@@ -54,14 +60,19 @@ public class UserController {
         User resultUser = userService.login(user);
         log.info("request: user/login , user: " + user.toString());
         if (resultUser == null) {
-            request.setAttribute("user", user);
-            request.setAttribute("errorMsg", "请认真核对账号、密码！");
-            return "login";
+//            request.setAttribute("user", user);
+//            request.setAttribute("errorMsg", "请认真核对账号、密码！");
+//            return "login";
+            return ResultGenerator.genFailResult("请认真核对账号、密码！");
         } else {
-            HttpSession session = request.getSession();
-            session.setAttribute("currentUser", resultUser);
-            MDC.put("userName", user.getUserName());
-            return "redirect:/main.jsp";
+//            HttpSession session = request.getSession();
+//            session.setAttribute("currentUser", resultUser);
+//            MDC.put("userName", user.getUserName());
+//            return "redirect:/main.jsp";
+            resultUser.setPassword("PASSWORD");
+            Map data = new HashMap();
+            data.put("currentUser", resultUser);
+            return ResultGenerator.genSuccessResult(data);
         }
     }
 
